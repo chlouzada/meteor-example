1b451b951d69d76c1fc526275017413bf96f5f73
carlos <chlouzada@gmail.com>
2021-09-11 13:23:03 -0300

    style: form de login

--- imports/ui/LoginForm.jsx
+++ imports/ui/LoginForm.jsx
@@ -11,29 +11,45 @@ export const LoginForm = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            required
-            onChange={(e) => setUsername(e.target.value)}
-          />
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
+        <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:mx-auto w-full mt-10 md:mt-0">
+          <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
+            Sign In
+          </h2>
+          <form onSubmit={handleSubmit}>
+            <div className="relative mb-4">
+              <label htmlFor="username" className="leading-7 text-sm text-gray-600">
+                Usernane
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="relative mb-4">
+              <label htmlFor="password" className="leading-7 text-sm text-gray-600">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="flex justify-center mt-8">
+              <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                Log In
+              </button>
+            </div>
+          </form>
         </div>
-
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            required
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Log In</button>
-      </form>
-    </div>
+      </div>
+    </section>
   );
 };
